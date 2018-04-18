a8aebf28eb88a1be5dce96f9d87f58606ffdc769
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-04-18 12:55:52 -0500

    funciones

--- Javascript/Variables.js
+++ Javascript/Variables.js
@@ -27,12 +27,45 @@ if (y) {
 
 
 // Json
-
+// objetos
 var usuario = {
     nombre: 'Jorge',
     apellido: 'Carrillo',
     cedula: '12132',
     edad: '22',
+    //imprimir:(),
 };
 console.log(usuario.nombre);
 console.log(usuario);
+delete usuario.edad;
+console.log(usuario);
+usuario.fechaNacimeinto =new Date();
+console.log(usuario);
+usuario.mascotas={};
+usuario.mascotas.nombre = 'Jorge';
+console.log(usuario);
+
+var arreglo = [1,'Jorge',true,undefined,null,new Date(),{nombre:'Jorge'},[12,12]];
+console.log("Arreglo");
+console.log(arreglo);
+
+function sumarNumero(numeroUno,numeroDos) {
+    return numeroUno + numeroDos;
+
+}
+console.log(sumarNumero(1,3));
+console.log(sumarNumero(1,2.444));
+
+var potenciaDeDosDeUnNumero = function (numero) { //no es necesario poner el nombre de la funcion
+return numero*numero;
+};
+var potenciaDeDosDeUnNumero = function (numero) {
+return numero*numero;
+};
+
+console.log(potenciaDeDosDeUnNumero(3,23,3,4,5,2));
+console.log(potenciaDeDosDeUnNumero(2));
+
+
+
+
